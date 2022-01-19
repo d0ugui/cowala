5a8b4f2a493721401e7d4634fb683346d18bd274
Douglas Oliveira <douglaspo_97@outlook.com>
2022-01-18 23:16:35 -0300

    finish validate.js

---
+++ algoritmos/validate.js
@@ -0,0 +1,14 @@
+function validate(dt, str) {
+  const now = new Date();
+  const item = new Date(dt);
+
+  item.setDate(item.getDate() + Number(str.replace(/\D/g, '')));
+  
+  if (item > now) {
+    console.log(`${item.toDateString()} ainda não expirou!`);
+  } else {
+    console.log(`${item.toDateString()} já expirou!`)
+  }
+}
+
+validate("2021-12-10T00:00:00.000Z", "180d");
